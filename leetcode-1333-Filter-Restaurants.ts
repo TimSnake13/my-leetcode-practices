5bce648ad08181e260957c8e86f61db6127f2e82
GoG <forsoapfc@gmail.com>
2021-02-19 11:01:43 +0800

    1333 cleanup

--- leetcode-1333-Filter-Restaurants.ts
+++ leetcode-1333-Filter-Restaurants.ts
@@ -1,44 +1,23 @@
-function swap(arr: number[][], index1: number, index2: number) {
-  let temp = arr[index1];
-  arr[index1] = arr[index2];
-  arr[index2] = temp;
-}
-
-function bubbleSortReverse(arr: number[][]) {
-  let endIndex = arr.length - 1;
-  while (endIndex > 0) {
-    let swaps = 0;
-    let currentIndex = 0;
-    while (currentIndex < endIndex) {
-      if (arr[currentIndex][1] < arr[currentIndex + 1][1]) {
-        swap(arr, currentIndex, currentIndex + 1);
-        swaps++;
-      }
-      currentIndex++;
-    }
-    if (swaps === 0) break;
-    endIndex--;
-  }
-  return arr;
-}
-
 export default function filterRestaurants(
   restaurants: number[][],
   veganFriendly: number,
   maxPrice: number,
   maxDistance: number
 ): number[] {
-  // 1. Filtering
-  const filtered = restaurants.filter(
-    (restaurant: number[]) =>
-      restaurant[2] === veganFriendly &&
-      restaurant[3] <= maxPrice &&
-      restaurant[4] <= maxDistance
-  );
-  console.log(filtered);
-  // 2. Rank from highest rating to lowest, return only IDs
-  const result = bubbleSortReverse(filtered).map((v) => v[0]);
-  console.log(result);
-
-  return result;
+  return restaurants
+    .filter((restaurant) => {
+      return (
+        (!veganFriendly || veganFriendly === restaurant[2]) &&
+        maxPrice >= restaurant[3] &&
+        maxDistance >= restaurant[4]
+      );
+    })
+    .sort((a, b) => {
+      if (a[1] !== b[1]) {
+        return b[1] - a[1];
+      } else {
+        return b[0] - a[0];
+      }
+    })
+    .map((restaurant) => restaurant[0]);
 }
